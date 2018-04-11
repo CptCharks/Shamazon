a5ea8574c4cbff77923ea79ef4b7bcf359757edc
Seth <Seth@DESKTOP-OH5MN3G>
2018-04-11 17:52:03 -0500

    dded Methods for adding users and products to the database

--- Shamazon/src/shamazon/Shamazon.java
+++ Shamazon/src/shamazon/Shamazon.java
@@ -5,38 +5,13 @@
  */
 package shamazon;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 /**
  *
  * @author Seth
  */
 public class Shamazon {
     
-      public static void main(String[] args) throws SQLException, ClassNotFoundException{
-           
-    /**
-     * @param args the command line arguments
-     * @throws java.lang.ClassNotFoundException
-     * @throws java.sql.SQLException
-     */
-        String userName ="ShamAdmin";
-        String b = "sdkgv";
-        String password ="Shamazon123";
-        String url ="jdbc:sqlserver://shamazondb.cvll5p9eet0l.us-east-2.rds.amazonaws.com:1433;databaseName=ShamazonDB";
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
-        Connection conn = DriverManager.getConnection(url, userName, password);
-        System.out.println("test");
-        String query = "select * from UserInfo";
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        rs.next();
-        if (rs.getString("UserName")== b){
-        System.out.println(rs.getString("UserName"));
-        }
+      public static void main(String[] args){
+          
       }
-}
+      }
