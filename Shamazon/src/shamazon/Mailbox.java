e1f6dce9f19c326878bf85c931e99b7e524f819b
tungstenturtle <lukelarew@gmail.com>
2018-04-25 16:45:38 -0500

    Implement Mailbox

--- Shamazon/src/shamazon/Mailbox.java
+++ Shamazon/src/shamazon/Mailbox.java
@@ -8,11 +8,12 @@ import java.util.*;
 import java.lang.String;
 import java.awt.image.*;
 import java.io.Serializable;
+import java.sql.SQLException;
 
 
 /**
- *
- * 
+ * Holds a list of messages which have been sent to the owner of this Mailbox.
+ * Supplies methods to send and receive messages.
  */
 public class Mailbox implements Serializable
 {
@@ -27,31 +28,54 @@ public class Mailbox implements Serializable
         this.owner = owner;
     }
     
-    public void SendMessage(UserAccount receiver, Message message)
+    /**
+     * Sends a message to another user's mailbox
+     * @param receiver the receiver's username
+     * @param title the title of the message
+     * @param messageText the text of the message
+     */
+    public void SendMessage(String receiver, String title, String messageText)
     {
-        Mailbox receivingMailbox = receiver.GetMailbox();
+        UserAccount receiverAccount;
+        try
+        {
+            receiverAccount = DatabaseManager.GetUserAccountByName(receiver);
+        }
+        catch(SQLException e)
+        {
+            return;
+        }
+        
+        Mailbox receivingMailbox = receiverAccount.GetMailbox();
+        
+        Message message = new Message(messageText, title, this.owner, receiverAccount);
+        receivingMailbox.AddMessage(message);
     }
     
-    public void RecieveMessage(UserAccount sender, Message message)
-    {
-        
-    }
-                
+    /**
+     * Adds a message to the mailbox
+     * @param message the message to be added
+     */
     public void AddMessage(Message message)
     {
         inbox.add(message);
     }
     
+    /**
+     * Removes a message from a mailbox
+     * @param message the message to be removed
+     */
     public void RemoveMessage(Message message)
     {
         inbox.remove(message);
     }
     
-    
+    /**
+     * 
+     * @return the UserAccount which owns this mailbox
+     */
     public UserAccount GetOwner()
     {
         return this.owner;
     }
-    
-    
 }
