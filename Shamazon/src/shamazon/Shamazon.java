aab6c314b5dcf19f1bffca934ff8f149d9ad3e01
Seth <Seth@DESKTOP-OH5MN3G>
2018-04-25 16:55:12 -0500

    Implemented Login

--- Shamazon/src/shamazon/Shamazon.java
+++ Shamazon/src/shamazon/Shamazon.java
@@ -22,32 +22,36 @@ public class Shamazon {
      */
     public static void main(String[] args) throws SQLException
     {
-        
-        JDialog dialog = new JDialog();
-        UserAccount newUser = UserAccountManager.CreateUserAccount(dialog);
-        
-        if(newUser != null)
-            DatabaseManager.AddObjectToDatabase(newUser, "UserAccounts");
-        
-        try
-        {
-            newUser = (UserAccount)DatabaseManager.GetUpdatedObjectFromDatabase(newUser, "UserAccounts");
+ /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        catch(SQLException e)
-        {
-            
-        }
-        
-        try
-        {
-            dialog = new JDialog();
-            newUser = UserAccountManager.EditUserAccount(dialog, newUser);
-        }
-        catch(Exception e)
-        {
-            e.toString();
-        }
-        
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginFrame().setVisible(true);
+            }
+        });
+    
         
 //      
 //        Listing list1 = new Listing("Polly Locket",null,"Tis #######",10,null, null);
