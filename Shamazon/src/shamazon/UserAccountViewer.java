28f5987b82edbcb660353f1334ea349faddb8204
tungstenturtle <lukelarew@gmail.com>
2018-04-25 20:26:44 -0500

    Update UserAccountViewer GUI

--- Shamazon/src/shamazon/UserAccountViewer.java
+++ Shamazon/src/shamazon/UserAccountViewer.java
@@ -36,49 +36,59 @@ public class UserAccountViewer extends javax.swing.JPanel
     private void initComponents()
     {
 
-        jLabel7 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
         avatarLabel = new javax.swing.JLabel();
         usernameLabel = new javax.swing.JLabel();
         nameLabel = new javax.swing.JLabel();
         addressLabel = new javax.swing.JLabel();
         emailLabel = new javax.swing.JLabel();
         creditCardLabel = new javax.swing.JLabel();
+        purchasedListingsButton = new javax.swing.JButton();
+        postedListingsButton = new javax.swing.JButton();
 
         setPreferredSize(new java.awt.Dimension(580, 175));
-
-        jLabel7.setText("Credit Card Number");
-
-        jLabel4.setText("Name");
-
-        jLabel2.setText("Username");
-
-        jLabel5.setText("Address");
-
-        jLabel6.setText("Email");
 
         avatarLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ShamazonResources/ClientImages/ImageMissing.png"))); // NOI18N
         avatarLabel.setMaximumSize(new java.awt.Dimension(150, 150));
         avatarLabel.setMinimumSize(new java.awt.Dimension(150, 150));
         avatarLabel.setPreferredSize(new java.awt.Dimension(150, 150));
 
+        usernameLabel.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
         usernameLabel.setText("jLabel1");
-        usernameLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        usernameLabel.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "Username", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 16))); // NOI18N
 
+        nameLabel.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
         nameLabel.setText("jLabel1");
-        nameLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        nameLabel.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "Name", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 16))); // NOI18N
 
+        addressLabel.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
         addressLabel.setText("jLabel1");
-        addressLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        addressLabel.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "Address", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 16))); // NOI18N
 
+        emailLabel.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
         emailLabel.setText("jLabel1");
-        emailLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        emailLabel.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "Email", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 16))); // NOI18N
 
+        creditCardLabel.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
         creditCardLabel.setText("jLabel1");
-        creditCardLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        creditCardLabel.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)), "Credit Card Number", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 0, 16))); // NOI18N
+
+        purchasedListingsButton.setText("Purchased Listings");
+        purchasedListingsButton.addMouseListener(new java.awt.event.MouseAdapter()
+        {
+            public void mouseClicked(java.awt.event.MouseEvent evt)
+            {
+                purchasedListingsButtonMouseClicked(evt);
+            }
+        });
+
+        postedListingsButton.setText("Posted Listings");
+        postedListingsButton.addMouseListener(new java.awt.event.MouseAdapter()
+        {
+            public void mouseClicked(java.awt.event.MouseEvent evt)
+            {
+                postedListingsButtonMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -89,51 +99,53 @@ public class UserAccountViewer extends javax.swing.JPanel
                 .addComponent(avatarLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel5)
-                    .addComponent(addressLabel)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(usernameLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(nameLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 168, Short.MAX_VALUE)
+                        .addComponent(addressLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(postedListingsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel4)
-                            .addComponent(usernameLabel)
-                            .addComponent(nameLabel))
-                        .addGap(138, 138, 138)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(creditCardLabel)
-                            .addComponent(jLabel7)
-                            .addComponent(jLabel6)
-                            .addComponent(emailLabel))))
-                .addContainerGap(121, Short.MAX_VALUE))
+                        .addComponent(purchasedListingsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 68, Short.MAX_VALUE))
+                    .addComponent(creditCardLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(emailLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel6))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(usernameLabel)
                             .addComponent(emailLabel))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel7))
-                        .addGap(11, 11, 11)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(nameLabel)
                             .addComponent(creditCardLabel))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jLabel5)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(addressLabel))
                     .addComponent(avatarLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(postedListingsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(purchasedListingsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(16, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void purchasedListingsButtonMouseClicked(java.awt.event.MouseEvent evt)//GEN-FIRST:event_purchasedListingsButtonMouseClicked
+    {//GEN-HEADEREND:event_purchasedListingsButtonMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_purchasedListingsButtonMouseClicked
+
+    private void postedListingsButtonMouseClicked(java.awt.event.MouseEvent evt)//GEN-FIRST:event_postedListingsButtonMouseClicked
+    {//GEN-HEADEREND:event_postedListingsButtonMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_postedListingsButtonMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -141,12 +153,9 @@ public class UserAccountViewer extends javax.swing.JPanel
     private javax.swing.JLabel avatarLabel;
     private javax.swing.JLabel creditCardLabel;
     private javax.swing.JLabel emailLabel;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel nameLabel;
+    private javax.swing.JButton postedListingsButton;
+    private javax.swing.JButton purchasedListingsButton;
     private javax.swing.JLabel usernameLabel;
     // End of variables declaration//GEN-END:variables
 
